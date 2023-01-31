59ad1b1d5480399e786661d19580334f3625872b
Pablo <74419051+MrChova20@users.noreply.github.com>
2023-01-31 23:09:35 +0100

    explicaciones

--- GUI_Example.m
+++ GUI_Example.m
@@ -138,106 +138,84 @@ image1=get(handles.imag_processed,'UserData');
 
 %% Important parameters
 
-imageBlurSigma = 2; %cantidad de suavizado que se aplicará a la imagen de entrada.
-MinHoughPeakDistance = 5; % es la distancia mínima permitida entre los picos en la detección de ángulos en la transformada de Hough.
-HoughConvolutionLength = 40; %  tamaño de la línea que se utilizará para detectar regiones óseas en la imagen.
-HoughConvolutionDilate = 2; % cantidad de dilatación que se aplicará al kernel para la detección de huesos.
-BreakLineTolerance = 0.25; % la tolerancia para la detección del final de los huesos.
+sigmaSuavizadoImagen  = 2; %cantidad de suavizado que se aplicará a la imagen de entrada.
+distanciaMinPicosHough  = 5; % es la distancia mínima permitida entre los picos en la detección de ángulos en la transformada de Hough.
+tamLineaDeteccionHuesos  = 40; %  tamaño de la línea que se utilizará para detectar regiones óseas en la imagen.
+dilatacionDeteccionHuesos  = 2; % cantidad de dilatación que se aplicará al kernel para la detección de huesos.
+toleranciaDeteccionFinalHuesos  = 0.25; % la tolerancia para la detección del final de los huesos.
 breakPointDilate = 6; %es la cantidad de dilatación que se aplicará a los puntos detectados en el final de los huesos.
 
 %%%
 
-image_gray = (rgb2gray(image1)); % 1º se convierte a escala de grises con la función "rgb2gray"
+imagenGris  = (rgb2gray(image1)); % 1º se convierte a escala de grises con la función "rgb2gray"
 
 
 
-image_filtered = imfilter(image_gray, fspecial('gaussian', 10, imageBlurSigma), 'symmetric'); %aplica un filtro gaussiano a la imagen en escala de grises "image_gray" y guarda el resultado en la variable "image_filtered". El filtro gaussiano se crea con la función "fspecial" y tiene un tamaño de 10x10 y un sigma (desviación estándar) especificado por la variable "imageBlurSigma". El parámetro "symmetric" indica que se debe
+imagenFiltrada  = imfilter(imagenGris , fspecial('gaussian', 10, sigmaSuavizadoImagen ), 'symmetric'); %aplica un filtro gaussiano a la imagen en escala de grises "imagenGris " y guarda el resultado en la variable "imagenFiltrada ". El filtro gaussiano se crea con la función "fspecial" y tiene un tamaño de 10x10 y un sigma (desviación estándar) especificado por la variable "sigmaSuavizadoImagen ". El parámetro "symmetric" indica que se debe
                                                                                                 %utilizar una forma de simetría en el filtrado para mejorar la conservación de los bordes.
 
 
-boneEdges = edge(image_filtered, 'canny');%  detecta los bordes en la imagen "image_filtered" utilizando el algoritmo de Canny. El resultado es almacenado en la variable "boneEdges".
+bordesHuesos  = edge(imagenFiltrada , 'canny');%  detecta los bordes en la imagen "imagenFiltrada " utilizando el algoritmo de Canny. El resultado es almacenado en la variable "bordesHuesos ".
 
 
 
-boneEdges1 = bwmorph(boneEdges, 'close');%Esta linea de codigo utiliza la funcion "bwmorph" de Matlab, la cual permite realizar operaciones morfologicas en imágenes binarias. En este caso, se utiliza la operación "close" para cerrar pequeñas brechas o agujeros en los bordes detectados previamente en la imagen y unificar los contornos conectados. La salida se almacena en la variable "boneEdges1".
+bordesHuesos1 = bwmorph(bordesHuesos , 'close');%Esta linea de codigo utiliza la funcion "bwmorph" de Matlab, la cual permite realizar operaciones morfologicas en imágenes binarias. En este caso, se utiliza la operación "close" para cerrar pequeñas brechas o agujeros en los bordes detectados previamente en la imagen y unificar los contornos conectados. La salida se almacena en la variable "bordesHuesos1".
 
 
 
-edgeRegs = regionprops(boneEdges1, 'Area', 'PixelIdxList');%  Esta línea de código utiliza la función "regionprops" para identificar las regiones conectadas en la imagen "boneEdges1" y calcular propiedades de estas regiones. La propiedad "Area" se refiere al número de píxeles en cada región, mientras que la propiedad "PixelIdxList" es una lista de índices de los píxeles en cada región. Es decir, "edgeRegs" es una estructura que contiene información sobre las regiones conectadas en la imagen "boneEdges1".
-AreaList = sort(vertcat(edgeRegs.Area), 'descend'); %oncatena y ordena en orden descendente los valores de la propiedad "Area" de la estructura "edgeRegs" y los almacena en la matriz "AreaList".
-edgeRegs(~ismember(vertcat(edgeRegs.Area), AreaList(1:2))) = [];
-edgeimage = zeros(size(image_filtered, 1), size(image_filtered,2));%    GRAFICA 6 EN PROCESOOOOOOOOOOOOOO
-edgeimage(vertcat(edgeRegs.PixelIdxList)) = 1;
+regionesBordes = regionprops(bordesHuesos1, 'Area', 'PixelIdxList');%  Esta línea de código utiliza la función "regionprops" para identificar las regiones conectadas en la imagen "bordesHuesos1" y calcular propiedades de estas regiones. La propiedad "Area" se refiere al número de píxeles en cada región, mientras que la propiedad "PixelIdxList" es una lista de índices de los píxeles en cada región. Es decir, "regionesBordes" es una estructura que contiene información sobre las regiones conectadas en la imagen "bordesHuesos1".
+listaAreas  = sort(vertcat(regionesBordes.Area), 'descend'); %oncatena y ordena en orden descendente los valores de la propiedad "Area" de la estructura "regionesBordes" y los almacena en la matriz "listaAreas ".
+regionesBordes(~ismember(vertcat(regionesBordes.Area), listaAreas (1:2))) = [];% elimina las regiones con áreas específicas (listaAreas (1:2)) de la variable "regionesBordes".
+imagenBordes = zeros(size(imagenFiltrada , 1), size(imagenFiltrada ,2));%crea una imagen binaria llamada "imagenBordes", donde los píxeles pertenecientes a las regiones en "regionesBordes" son marcados como 1.
+imagenBordes(vertcat(regionesBordes.PixelIdxList)) = 1;%aplica la transformada de Hough en la imagen binaria "imagenBordes". Los parámetros específicos incluyen una resolución para el parámetro "Rho" y un rango de valores para el parámetro "Theta".
 
-% Do hough transform on edge image to find angles at which bone pieces are
-% found
-% Use max value of Hough transform vs angle to find angles at which lines
-% are oriented.  If there is more than one major angle contribution there
-% will be two peaks detected but only one peak if there is only one major
-% angle contribution (ie peaks here = number of located bones = Number of
-% breaks + 1)
-[H,T,R] = hough(edgeimage,'RhoResolution',1,'Theta',-90:2:89.5);
-maxHough = max(H, [], 1);
-HoughThresh = (max(maxHough) - min(maxHough))/2 + min(maxHough);
-[~, HoughPeaks] = findpeaks(maxHough,'MINPEAKHEIGHT',HoughThresh, 'MinPeakDistance', MinHoughPeakDistance);
 
-% Plot Hough detection results
-%figure(6)% MOSTRAR GRAFICA
-%plot(T, maxHough);
-hold on
-%plot([min(T) max(T)], [HoughThresh, HoughThresh], 'g');
-%plot(T(HoughPeaks), maxHough(HoughPeaks), 'rx', 'MarkerSize', 12, 'LineWidth', 2);
-hold off
-%xlabel('Theta Value'); ylabel('Max Hough Transform');
-%legend({'Max Hough Transform', 'Hough Peak Threshold', 'Detected Peak'});
-%title('Hough Detection Plot : Max Hough transform vs Theta');% MOSTRAR GRAFICA
+[H,T,R] = hough(imagenBordes,'RhoResolution',1,'Theta',-90:2:89.5);%permite detectar líneas en una imagen binaria. La función toma como entrada la imagen binaria imagenBordes y retorna tres matrices: H, T, y R.
+umbralHough = max(H, [], 1);% umbral para los valores de la transformada de Hough.
+peaksHough = (max(umbralHough) - min(umbralHough))/2 + min(umbralHough);%busca los picos en la variable umbralHough.
+[~, HoughPeaks] = findpeaks(umbralHough,'MINPEAKHEIGHT',peaksHough, 'MinPeakDistance', distanciaMinPicosHough );%encuentra los picos en los valores de la variable umbralHough que superan el umbral y cumplen con una distancia mínima especificada.
+
+
+hold on  %permite que los gráficos o dibujos se superpongan en una misma figura en vez de crear nuevas figuras para cada gráfico o dibujo. Esto es útil cuando se desea comparar varios gráficos o dibujos en una misma figu
+
+hold off %desactivar el modo de retener gráficos en una figura. Por defecto, MATLAB suele sobrescribir los gráficos en la misma figura, pero cuando se activa "hold on", se pueden mantener y superponer varios gráficos en la misma figura. Al utilizar "hold off", se desactiva este comportamiento y los siguientes gráficos se dibujarán en una figura nueva o sobrescribirán los existentes en la misma figura.
 
 
 
-%%2nd CT 8th sem
 
-% Locate site of break
+
+
 if numel(HoughPeaks) > 1;
-    BreakStack = zeros(size(image_filtered, 1), size(image_filtered, 2), numel(HoughPeaks));
-    % Convolute edge image with line of detected angle from hough transform
+    BreakStack = zeros(size(imagenFiltrada , 1), size(imagenFiltrada , 2), numel(HoughPeaks));
+
     for m = 1:numel(HoughPeaks);
 
-        boneKernel = strel('line', HoughConvolutionLength, T(HoughPeaks(m)));
-        kern = double(bwmorph(boneKernel.getnhood(), 'dilate', HoughConvolutionDilate));
-        BreakStack(:,:,m) = imfilter(edgeimage, kern).*edgeimage;
-        %figure(7) %MOSTRAR  IMAGEN
-        %imshow(BreakStack(:,:,m));
-        
+        boneKernel = strel('line', tamLineaDeteccionHuesos , T(HoughPeaks(m)));
+        kern = double(bwmorph(boneKernel.getnhood(), 'dilate', dilatacionDeteccionHuesos ));
+        BreakStack(:,:,m) = imfilter(imagenBordes, kern).*imagenBordes;
+
     end
 
-    % Take difference between convolution images.  Where this crosses zero
-    % (within tolerance) should be where the break is.  Have to filter out
-    % regions elsewhere where the bone simply ends.
     
-    
-    brimage = abs(diff(BreakStack, 1, 3)) < BreakLineTolerance*max(BreakStack(:)) & edgeimage > 0;
-    [BpY, BpX] = find(abs(diff(BreakStack, 1, 3)) < BreakLineTolerance*max(BreakStack(:)) & edgeimage > 0);
+    brimage = abs(diff(BreakStack, 1, 3)) < toleranciaDeteccionFinalHuesos *max(BreakStack(:)) & imagenBordes > 0;
+    [BpY, BpX] = find(abs(diff(BreakStack, 1, 3)) < toleranciaDeteccionFinalHuesos *max(BreakStack(:)) & imagenBordes > 0);
     brimage = bwmorph(brimage, 'dilate', breakPointDilate);
-    %figure(8);% MOSTRAR IMAGE
-    %imshow(brimage);% MOSTRAR IMAGE
+
     brReg = regionprops(brimage, 'Area', 'MajorAxisLength', 'MinorAxisLength', ...
         'Orientation', 'Centroid');
     brReg(vertcat(brReg.Area) ~= max(vertcat(brReg.Area))) = [];
 
-    % Calculate bounding ellipse
-    % CIRCULOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
     brReg.EllipseCoords = zeros(100, 2);
     t = linspace(0, 2*pi, 100);
     brReg.EllipseCoords(:,1) = brReg.Centroid(1) + brReg.MajorAxisLength/2*cos(t - brReg.Orientation);
     brReg.EllipseCoords(:,2) = brReg.Centroid(2) + brReg.MinorAxisLength/2*sin(t - brReg.Orientation);
 
 else
-    brReg = [];      %% No Fracture points are there
+    brReg = [];    
 
 end
 
-% Draw ellipse around break location
-%figure(9)% MOSTRAR IMAGE
 hold on
 colormap('gray')
 if ~isempty(brReg)
